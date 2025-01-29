4de94bc732f89b7b0e4cd90d280962911adb1661
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-01-29 16:06:32 +0300

    Initial Commit

---
+++ CineScope/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 1/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
