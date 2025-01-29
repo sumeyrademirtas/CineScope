5c5043765087d1ca25a5f776f6b59dee07b57640
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-01-29 19:54:17 +0300

    feat(api): add TmdbApi enum for handling TMDB endpoints
    # Implemented `TmdbApi` enum conforming to `TargetType` for Moya integration
    # Added cases for fetching popular, top-rated, now playing, and upcoming movies
    # Configured API requests with base URL, path, HTTP method, and query parameters
    # Included `ApiConstants` for managing API key and host dynamically

---
+++ CineScope/Layers/Domain/Remotes/Movies/Models/Moya/TmdbApi.swift
@@ -0,0 +1,66 @@
+//
+//  TmdbApi.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 1/29/25.
+//
+
+import Foundation
+import Moya
+
+enum TmdbApi {
+    case getPopularMovies(page: Int)
+    case getTopRatedMovies(page: Int)
+    case getNowPlayingMovies(page: Int)
+    case getUpcomingMovies(page: Int)
+}
+
+extension TmdbApi: TargetType { //TargetType Moya Provider in protokolu. Bu protocol baseURL, path, method, task. headers i getiriyor.
+    
+    private var constants: ApiConstants {
+        return ApiConstants()
+    }
+    
+    var baseURL: URL {
+        return URL(string: constants.apiHost)! // TMDB API Base URL
+    }
+    
+    var path: String {
+        switch self {
+        case .getPopularMovies:
+            return "/movie/popular"
+        case .getTopRatedMovies:
+            return "/movie/top_rated"
+        case .getNowPlayingMovies:
+            return "/movie/now_playing"
+        case .getUpcomingMovies:
+            return "/movie/upcoming"
+        }
+    }
+    
+    var method: Moya.Method {
+        return .get // Benim kullandigim TMDB Apileri GET istegi kullaniyor.
+    }
+    
+    var task: Moya.Task {
+        switch self {
+            case .getPopularMovies(page: let page),
+                .getNowPlayingMovies(page: let page),
+                .getTopRatedMovies(page: let page),
+                .getUpcomingMovies(page: let page):
+            var parameters: [String: Any] = [:]
+            parameters["api_key"] = constants.apiKey
+            parameters["page"] = page
+            parameters["language"] = "en-US"
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.default)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return [
+            "Content-Type": "application/json"
+        ]
+    }
+    
+    
+}
