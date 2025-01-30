f0a889903cc86e17a33a8350ab07e0682462f082
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-01-30 12:05:55 +0300

    feat(UseCase): implement MovieUseCase with API fetching and Combine support
    # Added `MovieUseCase` protocol for defining movie fetching operations
    # Implemented `MovieUseCaseImpl` to handle API requests via `MovieService`
    # Integrated Combine framework for async data fetching with `AnyPublisher`
    # Used `Publishers.Zip4` to synchronize multiple API requests
    # Ensured all requests complete before returning a combined result

---
+++ CineScope/Layers/Domain/UseCase/Movies/MovieUseCase.swift
@@ -0,0 +1,64 @@
+//
+//  MovieUseCase.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 1/30/25.
+//
+
+import Combine
+import Foundation
+
+// MARK: - Protocol Definition
+
+protocol MovieUseCase {
+    func getPopularMovies(api: MovieApi) -> AnyPublisher<MovieResponse?, any Error>?
+    func getUpcomingMovies(api: MovieApi) -> AnyPublisher<MovieResponse?, Error>?
+    func getNowPlayingMovies(api: MovieApi) -> AnyPublisher<MovieResponse?, Error>?
+    func getTopRatedMovies(api: MovieApi) -> AnyPublisher<MovieResponse?, Error>?
+}
+
+struct MovieUseCaseImpl: MovieUseCase {
+    private let service: MovieService
+
+    /// Dependency Injection
+    init(service: MovieService) {
+        self.service = service
+    }
+
+    // sira [.popular, .upcoming, .nowPlaying, .topRated]
+    func fetchAllMovies() -> AnyPublisher<(MovieResponse?, MovieResponse?, MovieResponse?, MovieResponse?), Error>? {
+        if //cagrilarin hepsi basarili olursa kod calismaya devam eder, herhangi biri nil donerse fetchAllMovies nil doner.
+            let popularPublisher = getPopularMovies(api: .getPopularMovies(page: 1)),
+            let upcomingPublisher = getUpcomingMovies(api: .getUpcomingMovies(page: 1)),
+            let nowPlayingPublisher = getNowPlayingMovies(api: .getNowPlayingMovies(page: 1)),
+            let topRatedPublisher = getTopRatedMovies(api: .getTopRatedMovies(page: 1))
+        {
+            // Publishers.Zip4 tum istekler tamamlaninca sonuclari tek bir tuple icinde dondurur.
+            return Publishers.Zip4(popularPublisher, upcomingPublisher, nowPlayingPublisher, topRatedPublisher)
+            // Eğer map kullanmazsak, Zip4’ün sonucu doğrudan tuple olarak dönmez.
+                .map { popular, topRated, nowPlaying, upcoming in
+                    (popular, topRated, nowPlaying, upcoming)
+                }
+                .eraseToAnyPublisher()
+        }
+        return nil
+    }
+}
+
+extension MovieUseCaseImpl {
+    func getPopularMovies(api: MovieApi) -> AnyPublisher<MovieResponse?, any Error>? {
+        service.getPopularMovies(api: api)
+    }
+
+    func getUpcomingMovies(api: MovieApi) -> AnyPublisher<MovieResponse?, any Error>? {
+        service.getUpcomingMovies(api: api)
+    }
+
+    func getNowPlayingMovies(api: MovieApi) -> AnyPublisher<MovieResponse?, any Error>? {
+        service.getNowPlayingMovies(api: api)
+    }
+
+    func getTopRatedMovies(api: MovieApi) -> AnyPublisher<MovieResponse?, any Error>? {
+        service.getTopRatedMovies(api: api)
+    }
+}
