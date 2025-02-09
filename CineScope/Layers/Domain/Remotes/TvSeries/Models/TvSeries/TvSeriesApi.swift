c7f3500ab3f27e92cfa9a7fefc5885e5f3801e21
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-02-09 16:26:13 +0300

    feat(api): add TvSeriesApi for TV series endpoints using Moya
    # Added TvSeriesApi enum with cases for getAiringTodayTvSeries, getOnTheAirTvSeries, getPopularTvSeries, and getTopRatedTvSeries.
    # Conformed to TargetType by configuring baseURL, path, HTTP GET method, task parameters (api_key, page, language), and headers.

---
+++ CineScope/Layers/Domain/Remotes/TvSeries/Models/TvSeries/TvSeriesApi.swift
@@ -0,0 +1,65 @@
+//
+//  TvSeriesApi.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 2/7/25.
+//
+
+import Foundation
+import Moya
+
+enum TvSeriesApi {
+    case getAiringTodayTvSeries(page: Int)
+    case getOnTheAirTvSeries(page: Int)
+    case getPopularTvSeries(page: Int)
+    case getTopRatedTvSeries(page: Int)
+}
+
+extension TvSeriesApi: TargetType {
+    
+    private var constants: ApiConstants {
+        return ApiConstants()
+    }
+    
+    var baseURL: URL {
+        return URL(string: constants.apiHost)!
+    }
+    
+    var path: String {
+        switch self {
+        case .getAiringTodayTvSeries(page: let page):
+            return "/tv/airing_today"
+        case .getOnTheAirTvSeries(page: let page):
+            return "/tv/on_the_air"
+        case .getPopularTvSeries(page: let page):
+            return "/tv/popular"
+        case .getTopRatedTvSeries(page: let page):
+            return "/tv/top_rated"
+        }
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getAiringTodayTvSeries(page: let page),
+            .getOnTheAirTvSeries(page: let page),
+            .getPopularTvSeries(page: let page),
+            .getTopRatedTvSeries(page: let page):
+            var params: [String: Any] = [:]
+            params["api_key"] = constants.apiKey
+            params["page"] = page
+            params["language"] = "en-US"
+            return .requestParameters(parameters: params, encoding: URLEncoding.default)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return [
+            "Content-Type": "application/json"
+        ]
+    }
+    
+}
