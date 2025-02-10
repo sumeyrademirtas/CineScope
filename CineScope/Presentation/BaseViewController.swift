5bfd6389083a2775f6c9851226554c173b92ee1a
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-02-10 10:14:13 +0300

    feat(view): add BaseViewController with LoadingProtocol and remove redundant ViewController
    # Added BaseViewController subclassing UIViewController and conforming to LoadingProtocol,
    # including a lazy-initialized ProgressBar instance for loading animations.
    # Removed the unused ViewController.swift file.

---
+++ CineScope/Presentation/BaseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BaseViewController.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 2/7/25.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController, LoadingProtocol {
+    lazy var animator = ProgressBar()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
