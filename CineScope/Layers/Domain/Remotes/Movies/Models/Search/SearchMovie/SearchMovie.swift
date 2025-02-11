46f6e37b47f6ff2b8399439a0df4db5817f28d83
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-02-11 21:26:55 +0300

    feat(searchMovie): add SearchMovie and SearchMovieResponse models
    # Created SearchMovie struct conforming to Decodable with a computed fullPosterURL property.
    # Added SearchMovieResponse struct to parse API responses containing an array of SearchMovie.

---
+++ CineScope/Layers/Domain/Remotes/Movies/Models/Search/SearchMovie/SearchMovie.swift
@@ -0,0 +1,28 @@
+//
+//  Search.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 2/11/25.
+//
+
+import Foundation
+
+// MARK: Search Movie
+
+struct SearchMovie: Decodable {
+    let posterPath: String
+
+    var fullPosterURL: String {
+        let baseURL = "https://image.tmdb.org/t/p/w500" // Resim için temel URL
+        return "\(baseURL)\(posterPath)"
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case posterPath = "poster_path"
+    }
+}
+
+// MARK: SearchMovieResponse
+struct SearchMovieResponse: Decodable {
+    let results: [SearchMovie]
+}
