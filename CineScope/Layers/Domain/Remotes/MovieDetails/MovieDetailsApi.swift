446dfcc1375ec6f948d3aef916cfab80ceb1ccd5
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-02-15 21:28:16 +0300

    feat(movie-details-api): add MovieDetailsApi for fetching movie details
    # Implemented MovieDetailsApi enum to handle API requests for movie details.
    # Defined endpoint path dynamically using movie ID.
    # Configured GET request with query parameters including API key and language.

---
+++ CineScope/Layers/Domain/Remotes/MovieDetails/MovieDetailsApi.swift
@@ -0,0 +1,51 @@
+//
+//  MovieDetailsApi.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 2/15/25.
+//
+
+import Foundation
+import Moya
+
+enum MovieDetailsApi {
+    case getMovieDetails(movieId: Int)
+}
+
+extension MovieDetailsApi: TargetType {
+    var baseURL: URL {
+        return URL(string: constants.apiHost)!
+    }
+    
+    var path: String {
+        switch self {
+        case .getMovieDetails(let movieId):
+            return "/movie/\(movieId)"
+        }
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getMovieDetails(movieId: let movieId):
+            var params: [String: Any] = [:]
+            params["api_key"] = constants.apiKey
+            params["language"] = "en-US"
+            params["movie_id"] = movieId
+            return .requestParameters(parameters: params, encoding: URLEncoding.default)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return [
+            "Content-Type": "application/json"
+        ]
+    }
+    
+    private var constants: ApiConstants {
+        return ApiConstants()
+    }
+}
