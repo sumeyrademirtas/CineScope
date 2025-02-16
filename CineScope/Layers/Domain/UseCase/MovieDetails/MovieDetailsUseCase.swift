d228d1d5d8619e3e5f6f7908b4565885a6af40ab
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-02-16 10:51:09 +0300

    feat(movie-details-usecase): implement MovieDetailsUseCase for fetching movie details
    # Added `MovieDetailsUseCase` protocol to define contract for fetching movie details.
    # Implemented `MovieDetailsUseCaseImpl` with a dependency on `MovieDetailsService`.
    # Introduced `fetchMovieDetails(movieId:)` to retrieve movie details using API call.

---
+++ CineScope/Layers/Domain/UseCase/MovieDetails/MovieDetailsUseCase.swift
@@ -0,0 +1,37 @@
+//
+//  MovieDetailsUseCase.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 2/16/25.
+//
+
+import Foundation
+import Combine
+
+// MARK: - Protocol Definition
+protocol MovieDetailsUseCase {
+    func fetchMovieDetails(movieId: Int) -> AnyPublisher<MovieDetails?, Error>?
+}
+
+struct MovieDetailsUseCaseImpl: MovieDetailsUseCase {
+
+    
+    private let service: MovieDetailsService
+    
+    init(service: MovieDetailsService) {
+        self.service = service
+    }
+    
+    func fetchMovieDetails(movieId: Int) -> AnyPublisher<MovieDetails?, any Error>? {
+        guard let movieDetailsPublisher = getMovieDetails(api: .getMovieDetails(movieId: movieId)) else { return nil }
+        return movieDetailsPublisher.eraseToAnyPublisher()
+    }
+    
+}
+
+
+extension MovieDetailsUseCaseImpl {
+    func getMovieDetails(api: MovieDetailsApi) -> AnyPublisher<MovieDetails?, any Error>? {
+        service.getMovieDetails(api: api)
+    }
+}
