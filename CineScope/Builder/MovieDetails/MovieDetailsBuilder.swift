707eb2942450f538e0081f0782e9dc4c7d43f89e
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-02-17 20:39:02 +0300

    feat(builder): add MovieDetailsBuilder for modular construction of MovieDetailsVC
    # Implemented MovieDetailsBuilder to handle dependency injection
    # Configures MovieDetailsVC with service, use case, view model, and provider
    # Ensures movieId is set via configure method before presenting the view

---
+++ CineScope/Builder/MovieDetails/MovieDetailsBuilder.swift
@@ -0,0 +1,29 @@
+//
+//  MovieDetailsBuilder.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 2/17/25.
+//
+
+import Foundation
+import Moya
+import UIKit
+
+protocol MovieDetailsBuilder {
+    func build(movieId: Int) -> UIViewController
+}
+
+struct MovieDetailsBuilderImpl: MovieDetailsBuilder {
+    func build(movieId: Int) -> UIViewController {
+        
+        let service = MovieDetailsServiceImpl()
+        let useCase = MovieDetailsUseCaseImpl(service: service)
+        let viewModel = MovieDetailsVMImpl(useCase: useCase)
+        let provider = MovieDetailsProviderImpl()
+
+        let vc = MovieDetailsVC(viewModel: viewModel, provider: provider)
+        vc.configure(movieId: movieId) // 🔥 Burada configure çağrılıyor
+
+        return vc
+    }
+}
