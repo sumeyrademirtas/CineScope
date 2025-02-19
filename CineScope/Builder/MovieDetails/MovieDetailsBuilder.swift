a2070e4a66db371a33783fd5728739c9d29dc553
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-02-19 21:10:32 +0300

    refactor(MovieDetailsBuilder): integrate MovieVideosService into use case
    # Added MovieVideosServiceImpl to support fetching movie trailers
    # Updated MovieDetailsUseCaseImpl initialization to include videoService
    # Enables fetching and displaying trailers in MovieDetails screen

--- CineScope/Builder/MovieDetails/MovieDetailsBuilder.swift
+++ CineScope/Builder/MovieDetails/MovieDetailsBuilder.swift
@@ -17,7 +17,8 @@ struct MovieDetailsBuilderImpl: MovieDetailsBuilder {
     func build(movieId: Int) -> UIViewController {
         
         let service = MovieDetailsServiceImpl()
-        let useCase = MovieDetailsUseCaseImpl(service: service)
+        let videoService = MovieVideosServiceImpl()
+        let useCase = MovieDetailsUseCaseImpl(service: service, videoService: videoService)
         let viewModel = MovieDetailsVMImpl(useCase: useCase)
         let provider = MovieDetailsProviderImpl()
 
