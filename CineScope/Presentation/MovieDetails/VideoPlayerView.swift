2383e17ac75807b70d220193c1f88718e131f4bd
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-02-19 21:06:51 +0300

    feat(VideoPlayerView): add YouTube video player support using WKWebView
    # Introduced VideoPlayerView to embed YouTube videos using WKWebView
    # Configured WKWebView with a non-scrollable and responsive layout
    # Implemented loadYouTubeVideo function to load embedded YouTube videos
    # Used HTML iframe to support autoplay, controls, and full-screen options

---
+++ CineScope/Presentation/MovieDetails/VideoPlayerView.swift
@@ -0,0 +1,53 @@
+//
+//  VideoPlayerView.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 2/18/25.
+//
+
+import UIKit
+import WebKit
+
+class VideoPlayerView: UIView, WKNavigationDelegate {
+
+    private var webView: WKWebView!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupWebView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupWebView()
+    }
+
+    private func setupWebView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.navigationDelegate = self
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.scrollView.isScrollEnabled = false
+        addSubview(webView)
+
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: topAnchor),
+            webView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+
+    func loadYouTubeVideo(videoID: String) {
+        let embedHTML = """
+        <html>
+        <body style="margin:0px;padding:0px;overflow:hidden;background-color:black;">
+        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/\(videoID)?playsinline=1&autoplay=1&controls=1&modestbranding=1&rel=0&showinfo=0"
+        frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>
+        </iframe>
+        </body>
+        </html>
+        """
+        webView.loadHTMLString(embedHTML, baseURL: nil)
+    }
+}
