b570000d19d8439723ea24b4fe4224253b4de4fa
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-02-21 15:09:55 +0300

    feat: add MovieCreditsApi for fetching movie cast details
    # Implemented MovieCreditsApi to retrieve movie credits from TMDB.
    # Added getMovieCredits case to fetch cast details using movieId.
    # Configured request parameters including api_key and language.
    # Set up appropriate headers for API requests.

---
+++ CineScope/Layers/Domain/Remotes/Movie/MovieCredits/MovieCreditsApi.swift
@@ -0,0 +1,52 @@
+//
+//  MovieCreditsApi.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 2/21/25.
+//
+
+import Foundation
+import Moya
+
+enum MovieCreditsApi {
+    case getMovieCredits(movieId: Int)
+}
+
+extension MovieCreditsApi: TargetType {
+    
+    private var constants: ApiConstants {
+        return ApiConstants()
+    }
+    
+    var baseURL: URL {
+        return URL(string: constants.apiHost)!
+    }
+    
+    var path: String {
+        switch self {
+        case .getMovieCredits(let movieId):
+            return "movie/\(movieId)/credits"
+        }
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getMovieCredits(movieId: let movieId):
+            var params: [String: Any] = [:]
+            params["api_key"] = constants.apiKey
+            params["language"] = "en-US"
+            return .requestParameters(parameters: params, encoding: URLEncoding.default)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return [
+            "Content-Type": "application/json"
+        ]
+    }
+
+}
