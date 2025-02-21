2965f7c6a25ccfe8c77694a739e21648c9b3706c
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-02-21 15:24:33 +0300

    feat: add MovieCreditsService for fetching movie cast details
    # Implemented MovieCreditsService to retrieve movie credits from TMDB.
    # Added getMovieCredits function to fetch and decode MovieCredits response.
    # Integrated BaseMoyaProvider with NetworkLoggerPlugin for API request logging.
    # Handled JSON decoding and error management for network requests.

---
+++ CineScope/Layers/Domain/Remotes/Movie/MovieCredits/MovieCreditsService.swift
@@ -0,0 +1,52 @@
+//
+//  MovieCreditsService.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 2/21/25.
+//
+
+import Combine
+import Foundation
+import Moya
+
+protocol MovieCreditsService {
+    func getMovieCredits(api: MovieCreditsApi) -> AnyPublisher<MovieCredits?, Error>?
+}
+
+struct MovieCreditsServiceImpl: MovieCreditsService {
+    let provider = BaseMoyaProvider<MovieCreditsApi>(plugins: [NetworkLoggerPlugin(configuration:
+        .init(
+            formatter: .init(responseData: JSONResponseDataFormatter),
+            logOptions: .verbose
+        ))]
+    )
+}
+
+extension MovieCreditsServiceImpl {
+    func getMovieCredits(api: MovieCreditsApi) -> AnyPublisher<MovieCredits?, any Error>? {
+        return Future { promise in
+            provider.request(api) { result in
+                switch result {
+                case .success(let response):
+                    do {
+                        // JSON verisini decode et ve unwrap yap
+                        let decodedResponse = try JSONDecoder().decode(MovieCredits.self, from: response.data)
+                        promise(.success(decodedResponse)) // Başarıyla promise gönder
+                    } catch {
+                        promise(.failure(error)) // JSON decode hatası
+                    }
+                case .failure(let moyaError):
+                    switch moyaError {
+                    case .underlying(let error, _):
+                        print("Underlying error: \(error)")
+                        promise(.failure(error)) // Ağ hatası
+                    default:
+                        print("Moya error: \(moyaError)")
+                        promise(.failure(moyaError)) // Diğer Moya hataları
+                    }
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
