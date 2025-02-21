9c7552fb2c40cd6b79059080742b72715167f868
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-02-21 14:15:59 +0300

    feat: add MovieCredits model for cast details
    # Introduced Cast struct to handle movie cast information.
    # Added profilePathURL computed property to generate full profile image URLs.
    # Mapped JSON keys using CodingKeys for proper decoding.

--- CineScope/Layers/Domain/Remotes/Movie/MovieCredits/MovieCredits.swift
+++ CineScope/Layers/Domain/Remotes/Movie/MovieCredits/MovieCredits.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+// MARK: - Cast
+struct Cast: Decodable {
+    let adult: Bool?
+    let gender, id: Int?
+    let knownForDepartment, name, originalName: String?
+    let popularity: Double?
+    let profilePath: String?
+    let castID: Int?
+    let character, creditID: String?
+    let order: Int?
+    let department, job: String?
+    
+    var profilePathURL: String {
+        guard let profilePath else { return "" }
+        return "https://image.tmdb.org/t/p/w500\(profilePath)"
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case adult, gender, id
+        case knownForDepartment = "known_for_department"
+        case name
+        case originalName = "original_name"
+        case popularity
+        case profilePath = "profile_path"
+        case castID = "cast_id"
+        case character
+        case creditID = "credit_id"
+        case order, department, job
+    }
+}
