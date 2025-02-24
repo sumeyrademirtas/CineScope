c337fea2694776ff26f01b0c7c46cc4a4df08f28
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-02-24 14:23:25 +0300

    feat(service): add PersonTvCreditsService for fetching TV credits
    # Implemented `PersonTvCreditsService` protocol to handle TV credits fetching.
    # Created `PersonTvCreditsServiceImpl` using Moya provider with logging.
    # Added `getPersonTvCredits(api:)` function to fetch and decode TV credits data.
    # Included error handling for network issues and JSON decoding failures.

---
+++ CineScope/Layers/Domain/Remotes/Person/PersonTvCredits/PersonTvCreditsService.swift
@@ -0,0 +1,56 @@
+//
+//  PersonTvCreditsService.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 2/24/25.
+//
+
+import Foundation
+import Combine
+import Moya
+
+protocol PersonTvCreditsService {
+    func getPersonTvCredits(api: PersonTvCreditsApi) -> AnyPublisher<PersonTvCredits?, Error>?
+}
+
+struct PersonTvCreditsServiceImpl: PersonTvCreditsService {
+    let provider = BaseMoyaProvider<PersonTvCreditsApi>(plugins: [NetworkLoggerPlugin(configuration:
+        .init(
+            formatter: .init(responseData: JSONResponseDataFormatter),
+            logOptions: .verbose
+        ))]
+    )
+}
+
+extension PersonTvCreditsServiceImpl {
+    func getPersonTvCredits(api: PersonTvCreditsApi) -> AnyPublisher<PersonTvCredits?, any Error>? {
+        return Future { promise in
+            provider.request(api) { result in
+                switch result {
+                case .success(let response):
+                    print("Response data count: \(response.data.count)")
+                    if let jsonString = String(data: response.data, encoding: .utf8) {
+                        print("Raw API JSON: \(jsonString)")
+                    }
+                    do {
+                        // JSON verisini decode et ve unwrap yap
+                        let decodedResponse = try JSONDecoder().decode(PersonTvCredits.self, from: response.data)
+                        promise(.success(decodedResponse)) // Başarıyla promise gönder
+                    } catch {
+                        promise(.failure(error)) // JSON decode hatası
+                    }
+                case .failure(let moyaError):
+                    switch moyaError {
+                    case .underlying(let error, _):
+                        print("Underlying error: \(error)")
+                        promise(.failure(error)) // Ağ hatası
+                    default:
+                        print("Moya error: \(moyaError)")
+                        promise(.failure(moyaError)) // Diğer Moya hataları
+                    }
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
