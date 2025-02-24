3b4a99fc520708369b8e3ecc225bfeb699fa1ac8
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-02-24 14:21:35 +0300

    feat(service): implement PersonMovieCreditsService for fetching movie credits
    # Created `PersonMovieCreditsService` protocol with `getPersonMovieCredits` function.
    # Implemented `PersonMovieCreditsServiceImpl` to handle API requests using Moya.
    # Configured `BaseMoyaProvider` with `NetworkLoggerPlugin` for debugging.
    # Added error handling for JSON decoding and network failures.
    # Implemented Combine's `AnyPublisher` to return asynchronous API results.

---
+++ CineScope/Layers/Domain/Remotes/Person/PersonMovieCredits/PersonMovieCreditsService.swift
@@ -0,0 +1,56 @@
+//
+//  PersonMovieCreditsService.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 2/24/25.
+//
+
+import Foundation
+import Combine
+import Moya
+
+protocol PersonMovieCreditsService {
+    func getPersonMovieCredits(api: PersonMovieCreditsApi) -> AnyPublisher<PersonMovieCredits?, Error>?
+}
+
+struct PersonMovieCreditsServiceImpl: PersonMovieCreditsService {
+    let provider = BaseMoyaProvider<PersonMovieCreditsApi>(plugins: [NetworkLoggerPlugin(configuration:
+        .init(
+            formatter: .init(responseData: JSONResponseDataFormatter),
+            logOptions: .verbose
+        ))]
+    )
+}
+
+extension PersonMovieCreditsServiceImpl {
+    func getPersonMovieCredits(api: PersonMovieCreditsApi) -> AnyPublisher<PersonMovieCredits?, any Error>? {
+        return Future { promise in
+            provider.request(api) { result in
+                switch result {
+                case .success(let response):
+                    print("Response data count: \(response.data.count)")
+                    if let jsonString = String(data: response.data, encoding: .utf8) {
+                        print("Raw API JSON: \(jsonString)")
+                    }
+                    do {
+                        // JSON verisini decode et ve unwrap yap
+                        let decodedResponse = try JSONDecoder().decode(PersonMovieCredits.self, from: response.data)
+                        promise(.success(decodedResponse)) // Başarıyla promise gönder
+                    } catch {
+                        promise(.failure(error)) // JSON decode hatası
+                    }
+                case .failure(let moyaError):
+                    switch moyaError {
+                    case .underlying(let error, _):
+                        print("Underlying error: \(error)")
+                        promise(.failure(error)) // Ağ hatası
+                    default:
+                        print("Moya error: \(moyaError)")
+                        promise(.failure(moyaError)) // Diğer Moya hataları
+                    }
+                }
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
