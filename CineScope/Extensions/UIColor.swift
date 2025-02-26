b23515fc7a718d1ebdc0d96820ed29954a68d53b
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-02-26 18:10:25 +0300

    feat(theme): add brand color extension for consistent styling
    # Created UIColor extension to define the primary brand color.
    # Added brandDarkBlue as the main theme color using HSB values.
    # Ensured consistency in UI design by centralizing color definitions.

---
+++ CineScope/Extensions/UIColor.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 2/26/25.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+
+    // ana renk
+    static let brandDarkBlue = UIColor(hue: 0.65, saturation: 0.27, brightness: 0.18, alpha: 1.00)
+}
