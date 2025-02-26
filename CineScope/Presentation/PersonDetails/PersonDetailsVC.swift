70f95a773d15ad74028e1755bbf9ac3c88528d98
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-02-26 14:24:39 +0300

    feat(person-details): Implement `PersonDetailsVC` for managing person details UI
    # Created `PersonDetailsVC` to handle the person details screen.
    # Implemented `setupUI()` to initialize the collection view with Auto Layout constraints.
    # Integrated Combine-based `binding()` to connect `PersonDetailsVM` and `PersonDetailsProvider` outputs.
    # Added `navigateToMovieDetails()` for transitioning to `MovieDetailsVC` when a movie is selected.
    # Implemented `showError()` to display errors when data fetching fails.
    # Ensured `viewDidLoad()` triggers necessary API calls based on the provided person ID.

---
+++ CineScope/Presentation/PersonDetails/PersonDetailsVC.swift
@@ -0,0 +1,140 @@
+//
+//  PersonDetailsVC.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 2/25/25.
+//
+
+import Combine
+import Foundation
+import UIKit
+
+class PersonDetailsVC: BaseViewController {
+    // MARK: - Types
+
+    typealias P = PersonDetailsProvider
+    typealias V = PersonDetailsVM
+    
+    // MARK: - Properties
+
+    private var viewModel: V?
+    private var provider: (any P)?
+    
+    // Combine binding
+    private let inputVM = PassthroughSubject<PersonDetailsVMImpl.PersonDetailsVMInput, Never>()
+    private let inputPR = PassthroughSubject<PersonDetailsProviderImpl.PersonDetailsProviderInput, Never>()
+    private var cancellables = Set<AnyCancellable>()
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .orange
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        return cv
+    }()
+    
+    private var personId: Int?
+    
+    init(viewModel: V, provider: any P /* , personId: Int */ ) {
+        self.viewModel = viewModel
+        self.provider = provider
+//        self.personId = personId
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // This method sets the person ID after initialization
+    func configure(personId: Int) {
+        self.personId = personId
+    }
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .white
+        super.viewDidLoad()
+        print("PersonDetailsVC viewDidLoad")
+
+        setupUI()
+        binding()
+        inputPR.send(.setupUI(collectionView: collectionView))
+            
+        // Person ID ile fetch
+        if let id = personId {
+            inputVM.send(.fetchPersonDetails(personId: id))
+            inputVM.send(.fetchPersonMovieCredits(personId: id))
+            inputVM.send(.fetchPersonTvCredits(personId: id))
+        }
+    }
+}
+
+// MARK: - Setup UI
+
+extension PersonDetailsVC {
+    private func setupUI() {
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - Combine Binding
+
+extension PersonDetailsVC {
+    private func binding() {
+        // vm den gelen ciktilari dinlemece
+        let vmOutput = viewModel?.activityHandler(input: inputVM.eraseToAnyPublisher())
+        vmOutput?.sink(receiveValue: { [weak self] event in
+            switch event {
+            case .isLoading(let show):
+                self?.loading(isShow: show)
+            case .dataSource(let section):
+                self?.inputPR.send(.prepareCollectionView(data: section))
+            case .errorOccured(let msg):
+                self?.showError(message: msg)
+            }
+        }).store(in: &cancellables)
+        
+        // Provider Output
+        let providerOutput = provider?.activityHandler(input: inputPR.eraseToAnyPublisher())
+        providerOutput?.sink(receiveValue: { [weak self] event in
+            switch event {
+            case .didSelectMovie(let movieId):
+                print("📲 didSelectMovie triggered with movieId: \(movieId)")
+                self?.navigateToMovieDetails(movieId: movieId)
+            case .didSelectTvSeries(let tvSeriesId):
+                print("📲 didSelectTvSeries triggered with tvSeriesId: \(tvSeriesId)")
+            }
+        }).store(in: &cancellables)
+    }
+}
+
+
+// MARK: - Error Handling
+
+extension PersonDetailsVC {
+    /// Hata mesajını kullanıcıya gösterir
+    private func showError(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+}
+
+extension PersonDetailsVC {
+    private func navigateToMovieDetails(movieId: Int) {
+           let movieDetailsVC = MovieDetailsBuilderImpl().build(movieId: movieId)
+           movieDetailsVC.modalPresentationStyle = .fullScreen
+           movieDetailsVC.modalTransitionStyle = .crossDissolve
+           self.present(movieDetailsVC, animated: true, completion: nil)
+       }
+}
