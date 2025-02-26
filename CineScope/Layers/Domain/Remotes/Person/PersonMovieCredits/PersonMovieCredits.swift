3583f99b0fecea7248d679da4acda5e54b1eaaf5
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-02-26 14:14:44 +0300

    fix(person-details): Handle optional posterPath in PersonMovieCredits
    # Updated `posterPath` in `PersonMovieCredits` model to be optional (`String?`).
    # Modified `fullPosterURL` to safely handle `nil` values, preventing crashes.
    # This resolves cases where a person has missing `posterPath`, ensuring smooth rendering.

--- CineScope/Layers/Domain/Remotes/Person/PersonMovieCredits/PersonMovieCredits.swift
+++ CineScope/Layers/Domain/Remotes/Person/PersonMovieCredits/PersonMovieCredits.swift
@@ -15,12 +15,13 @@ struct PersonMovieCreditsResponse: Codable {
 // Filmde oynayan oyuncuların modeli
 struct PersonMovieCredits: Codable {
     let id: Int
-    let posterPath: String
+    let posterPath: String?
     
     var fullPosterURL: String {
-        let baseURL = "https://image.tmdb.org/t/p/w500" // Resim için temel URL
-        return "\(baseURL)\(posterPath)"
-    }
+           let baseURL = "https://image.tmdb.org/t/p/w500"
+           // Eğer posterPath nil ise boş string döndür.
+           return posterPath != nil ? "\(baseURL)\(posterPath!)" : ""
+       }
 
     enum CodingKeys: String, CodingKey {
         case id
