4a6b6466acbe2d8d177e533a8976487bce3d674d
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-02-28 13:58:25 +0300

    Update MovieDetailsBuilder.swift

--- CineScope/Builder/MovieDetails/MovieDetailsBuilder.swift
+++ CineScope/Builder/MovieDetails/MovieDetailsBuilder.swift
@@ -28,8 +28,7 @@ struct MovieDetailsBuilderImpl: MovieDetailsBuilder {
         let vc = MovieDetailsVC(viewModel: viewModel, provider: provider)
         vc.configure(movieId: movieId) // 🔥 Burada configure çağrılıyor
         let navController = UINavigationController(rootViewController: vc)
-
-
+        
         return navController
     }
 }
