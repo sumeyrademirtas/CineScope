c1745023b62eba9b0ce879f2921f37174258683b
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-03-03 13:26:24 +0300

    feat(models): add TvSeriesDetails model with computed firstAirYear property
    # Created TvSeriesDetails model to represent TV series details.
    # Added computed property firstAirYear to extract and return only the year from firstAirDate.
    # Implemented fullPosterURL to generate the full image URL.
    # Introduced TvGenre model for genre representation.
    # Mapped API response keys using CodingKeys.

---
+++ CineScope/Layers/Domain/Remotes/TvSeries/Models/TvSeriesDetails/TvSeriesDetails.swift
@@ -0,0 +1,57 @@
+//
+//  TvSeriesDetails.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 3/3/25.
+//
+
+import Foundation
+
+struct TvSeriesDetails: Decodable {
+    let id: Int?
+    let name: String?
+    let firstAirDate: String?
+    let overview: String?
+    let posterPath: String?
+    let voteAverage: Double?
+    let numberOfEpisodes: Int?
+    let numberOfSeasons: Int?
+    let genres: [TvGenre]?
+    
+    // Tam boyutlu poster URL'si döndürmek için computed property
+    var fullPosterURL: String {
+        guard let posterPath else { return "" }
+        return "https://image.tmdb.org/t/p/w500\(posterPath)"
+    }
+    
+    // 📌 First Air Date'i sadece yıl olarak döndürme
+    var firstAirYear: String {
+        guard let firstAirDate = firstAirDate else { return "N/A" }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        
+        if let date = dateFormatter.date(from: firstAirDate) {
+            let yearFormatter = DateFormatter()
+            yearFormatter.dateFormat = "yyyy"
+            return yearFormatter.string(from: date)
+        }
+        
+        return "N/A"
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case id, name, overview, genres
+        case firstAirDate = "first_air_date"
+        case posterPath = "poster_path"
+        case voteAverage = "vote_average"
+        case numberOfEpisodes = "number_of_episodes"
+        case numberOfSeasons = "number_of_seasons"
+    }
+}
+
+// MARK: - Genre
+struct TvGenre: Decodable {
+    let id: Int
+    let name: String
+}
