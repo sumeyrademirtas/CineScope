fc7e602abbf9ba76db1e3bd37575484c4ab8d49b
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-03-06 10:07:22 +0300

    feat(tvseriesdetails): added TvSeriesDetailsVC implementation
    # Created TvSeriesDetailsVC.swift to manage TV series details view
    # Implemented Combine bindings to handle data flow between ViewModel and Provider
    # Added collection view setup and UI constraints
    # Integrated API calls to fetch TV series details, credits, and videos
    # Implemented error handling and navigation to cast details

---
+++ CineScope/Presentation/TvSeriesDetails/TvSeriesDetailsVC.swift
@@ -0,0 +1,144 @@
+//
+//  TvSeriesDetailsVC.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 3/4/25.
+//
+
+import Combine
+import Foundation
+import UIKit
+
+class TvSeriesDetailsVC: BaseViewController {
+    deinit {
+        print("Destroy TvSeriesDetailsVC") // MARK: - Memory Leak Check
+    }
+
+    // MARK: - Types
+
+    typealias P = TvSeriesDetailsProvider
+    typealias V = TvSeriesDetailsVM
+
+    // MARK: - Properties
+
+    private var viewModel: V?
+    private var provider: (any P)?
+
+    private var tvSeriesId: Int?
+
+    // Combine binding
+    private let inputVM = PassthroughSubject<TvSeriesDetailsVMImpl.TvSeriesDetailsVMInput, Never>()
+    private let inputPR = PassthroughSubject<TvSeriesDetailsProviderImpl.TvSeriesDetailsProviderInput, Never>()
+    private var cancellables = Set<AnyCancellable>()
+
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.headerReferenceSize = CGSize(width: UIScreen.main.bounds.width, height: 250) // Header boyutu
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .brandDarkBlue
+
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+
+    // MARK: - Init
+
+    init(viewModel: V, provider: any P) {
+        self.viewModel = viewModel
+        self.provider = provider
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(tvSeriesId: Int) {
+        self.tvSeriesId = tvSeriesId
+    }
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.brandDarkBlue
+        collectionView.contentInsetAdjustmentBehavior = .never
+        //    Eğer Navigation Bar varsa, CollectionView başlangıçta aşağıya kayar (içeriğin üstü Navigation Bar’ın altından başlamaz).
+
+        setupUI()
+        binding()
+        inputPR.send(.setupUI(collectionView: collectionView))
+        // 🔥 Eğer `movieId` varsa detayları getir
+        if let tvSeriesId = tvSeriesId {
+            inputVM.send(.fetchTvSeriesDetails(tvSeriesId: tvSeriesId))
+            inputVM.send(.fetchTvSeriesCredits(tvSeriesId: tvSeriesId)) // Cast için de ekle
+            inputVM.send(.fetchTvSeriesVideos(tvSeriesId: tvSeriesId))
+        }
+        print("🛠️ CollectionView Delegate: \(String(describing: collectionView.delegate))")
+    }
+}
+
+// MARK: - Setup UI
+
+extension TvSeriesDetailsVC {
+    private func setupUI() {
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - Combine Binding
+
+extension TvSeriesDetailsVC {
+    private func binding() {
+        let vmOutput = viewModel?.activityHandler(input: inputVM.eraseToAnyPublisher())
+        vmOutput?.receive(on: DispatchQueue.main).sink {
+            [weak self] event in
+            switch event {
+            case .isLoading(let isShow):
+                self?.loading(isShow: isShow)
+            case .errorOccurred(let message):
+                self?.showError(message: message)
+            case .dataSource(let section):
+                self?.inputPR.send(.prepareCollectionView(data: section))
+            }
+        }.store(in: &cancellables)
+
+        let providerOutput = provider?.activityHandler(input: inputPR.eraseToAnyPublisher())
+        providerOutput?.sink {
+            [weak self] event in
+            switch event {
+            case .didToggleFavorite(let tvSeriesId, let isFavorite):
+                print("Favori Durumu Değişti: MovieID: \(tvSeriesId), Favori: \(isFavorite)")
+            case .didSelectCast(let personId):
+                print("Did select cast with personId: \(personId)")
+                self?.navigateToCastDetails(personId: personId)
+            }
+        }.store(in: &cancellables)
+    }
+}
+
+// MARK: - Error Handling
+
+extension TvSeriesDetailsVC {
+    private func showError(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+}
+
+extension TvSeriesDetailsVC {
+    private func navigateToCastDetails(personId: Int) {
+        let personDetailsVC = PersonDetailsBuilderImpl().build(personId: personId)
+        personDetailsVC.modalPresentationStyle = .pageSheet
+        personDetailsVC.modalTransitionStyle = .crossDissolve
+        present(personDetailsVC, animated: true, completion: nil)
+    }
+}
