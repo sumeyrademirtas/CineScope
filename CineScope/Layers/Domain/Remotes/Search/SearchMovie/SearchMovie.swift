aace4ba8e95a619477dbdef5645fb035aa61f57a
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-03-21 14:32:46 +0300

    feat(search): update SearchMovie model to include id and optional posterPath
    # added 'id' field to SearchMovie for navigation to detail pages
    # made 'posterPath' optional and updated 'fullPosterURL' to handle nil values
    # ensured compatibility with TMDB API structure

--- CineScope/Layers/Domain/Remotes/Search/SearchMovie/SearchMovie.swift
+++ CineScope/Layers/Domain/Remotes/Search/SearchMovie/SearchMovie.swift
@@ -10,18 +10,21 @@ import Foundation
 // MARK: Search Movie
 
 struct SearchMovie: Decodable {
-    let posterPath: String
+    let posterPath: String?
     let name: String
+    let id: Int
     
 
-    var fullPosterURL: String {
-        let baseURL = "https://image.tmdb.org/t/p/w500" // Resim için temel URL
+    var fullPosterURL: String? {
+        guard let posterPath = posterPath, !posterPath.isEmpty else { return nil }
+        let baseURL = "https://image.tmdb.org/t/p/w500"
         return "\(baseURL)\(posterPath)"
     }
     
     enum CodingKeys: String, CodingKey {
         case posterPath = "poster_path"
         case name = "title"
+        case id
     }
 }
 
