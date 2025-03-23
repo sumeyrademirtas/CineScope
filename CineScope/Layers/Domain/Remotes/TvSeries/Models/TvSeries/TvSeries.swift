7c92dfe131fa7f0dc0e1d1c42f6c4b808c2e127e
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-03-23 11:56:35 +0300

    feat(model): add trending case to TvSeriesCategory enum
    # added .trending to TvSeriesCategory to support trending TV shows
    # updated orderedCategories to include trending as first item
    # adjusted displayName to return empty string for trending section

--- CineScope/Layers/Domain/Remotes/TvSeries/Models/TvSeries/TvSeries.swift
+++ CineScope/Layers/Domain/Remotes/TvSeries/Models/TvSeries/TvSeries.swift
@@ -8,29 +8,25 @@
 import Foundation
 
 // MARK: - Tv Series
+
 struct TvSeries: Decodable {
     let posterPath: String?
     let id: Int
     let name: String
-    
-//    var fullPosterURL: String {
-//        let baseUrl = "https://image.tmdb.org/t/p/w500"
-//        return "\(baseUrl)\(posterPath)"
-//    }
-    
+
     var fullPosterURL: String? {
         guard let posterPath = posterPath else { return nil }
         return "https://image.tmdb.org/t/p/w500\(posterPath)"
     }
-    
+
     enum CodingKeys: String, CodingKey {
         case posterPath = "poster_path"
         case id, name
     }
 }
 
-
 // MARK: - TvSeriesResponse
+
 struct TvSeriesResponse: Decodable {
     let results: [TvSeries]
 }
@@ -38,13 +34,14 @@ struct TvSeriesResponse: Decodable {
 enum TvSeriesCategory: String {
     case airingToday = "airing_today"
     case onTheAir = "on_the_air"
-    case popular = "popular"
+    case popular
     case topRated = "top_rated"
-    
+    case trending
+
     static var orderedCategories: [TvSeriesCategory] {
-        return [.airingToday, .onTheAir, .popular, .topRated]
+        return [.trending, .airingToday, .onTheAir, .popular, .topRated]
     }
-    
+
     var displayName: String {
         switch self {
         case .airingToday:
@@ -55,7 +52,8 @@ enum TvSeriesCategory: String {
             return "Popular"
         case .topRated:
             return "Top Rated"
+        case .trending:
+            return ""
         }
     }
 }
-
