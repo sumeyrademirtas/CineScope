d84720d76ce946b73697dd50ed921e87c2bde808
Sumeyra Demirtas <smyrdmrts@gmail.com>
2025-03-25 17:44:25 +0300

    feat(genreMovie): add GenreMovieApi for fetching movie genres from TMDB
    # defined Moya target for /genre/movie/list endpoint
    # includes baseURL, path, method, task, and headers
    # used to retrieve available movie genres

---
+++ CineScope/Layers/Domain/Remotes/Genres/MovieGenre/GenreMovieApi.swift
@@ -0,0 +1,48 @@
+//
+//  MovieGenreApi.swift
+//  CineScope
+//
+//  Created by Sümeyra Demirtaş on 3/25/25.
+//
+
+import Foundation
+import Moya
+
+enum GenreMovieApi {
+    case getMovieGenres
+}
+
+extension GenreMovieApi: TargetType {
+    
+    private var constants: ApiConstants {
+        return ApiConstants()
+    }
+    
+    var baseURL: URL {
+        return URL(string: constants.apiHost)!
+    }
+    
+    var path: String {
+        switch self {
+        case .getMovieGenres:
+            return "/genre/movie/list"
+        }
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var task: Moya.Task {
+        var params: [String: Any] = [:]
+        params["api_key"] = constants.apiKey
+        params["language"] = "en-US"
+        return .requestParameters(parameters: params, encoding: URLEncoding.default)
+    }
+    
+    var headers: [String : String]? {
+        return [
+            "Content-Type": "application/json"
+        ]
+    }
+}
